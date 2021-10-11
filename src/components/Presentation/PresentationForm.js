d675789eff2116961af7acf994ae3de397229217
normanvnfc <norman@venuerific.com>
2021-10-11 21:21:03 +0700

    Adding division field

--- src/components/Presentation/PresentationForm.js
+++ src/components/Presentation/PresentationForm.js
@@ -3,6 +3,7 @@ import { useForm } from "./useForm";
 
 const PresentationForm = () => {
   const [achievementLists, setAchievementLists] = useState([{ achievement: "" }]);
+  const [divisionLists, setDivisionLists] = useState([{ role: "", finished: "", wip: "", goals: "" }]);
   const [values, handleChange] = useForm({ title: '' })  
 
   const submitHandle = (e) => {
@@ -10,6 +11,7 @@ const PresentationForm = () => {
     const data = {
       title: values.title,      
       achievements: achievementLists,
+      division: divisionLists
     }
     console.log(data);
   }
@@ -20,6 +22,11 @@ const PresentationForm = () => {
     achievementValues[index][e.target.name] = e.target.value
     setAchievementLists(achievementValues);
   };
+  const handleDivisionInputChange = (e, index) => {
+    const divisiontValues = [...divisionLists]
+    divisiontValues[index][e.target.name] = e.target.value
+    setDivisionLists(divisiontValues);
+  };
 
   // // handle click event of the Remove button
   const handleRemoveClick = index => {
@@ -27,10 +34,18 @@ const PresentationForm = () => {
     currentAchievement.splice(index, 1);
     setAchievementLists(currentAchievement);
   };
+  const handleRemoveDivision = index => {
+    const currentDivision = [...divisionLists];
+    currentDivision.splice(index, 1);
+    setDivisionLists(currentDivision);
+  };
 
   // // handle click event of the Add button
   const handleAddClick = () => {
     setAchievementLists([...achievementLists, { achievement: "" }]);
+  };
+  const handleAddDivision = () => {
+    setDivisionLists([...divisionLists, { role: "", finished: "", wip: "", goals: "" }]);
   };
   return (
     <div>
@@ -57,6 +72,41 @@ const PresentationForm = () => {
                 {achievementLists.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
                 {achievementLists.length !== 1 &&
                   <button onClick={() => handleRemoveClick(i)}>Remove</button>}
+              </div>
+            </div>
+          );
+        })}
+        {divisionLists.map((divisionList, i) => {
+          return (
+            <div key={i} className="box">
+              <input
+                name="role"
+                placeholder="Role"
+                value={divisionList.role}
+                onChange={e => handleDivisionInputChange(e, i)}
+              />
+              <input
+                name="finished"
+                placeholder="Finished"
+                value={divisionList.finished}
+                onChange={e => handleDivisionInputChange(e, i)}
+              />
+              <input
+                name="wip"
+                placeholder="WIP"
+                value={divisionList.wip}
+                onChange={e => handleDivisionInputChange(e, i)}
+              />
+              <input
+                name="goals"
+                placeholder="Goals"
+                value={divisionList.goals}
+                onChange={e => handleDivisionInputChange(e, i)}
+              />
+              <div className="btn-box">
+                {divisionLists.length - 1 === i && <button onClick={handleAddDivision}>Add</button>}
+                {divisionLists.length !== 1 &&
+                  <button onClick={() => handleRemoveDivision(i)}>Remove</button>}
               </div>
             </div>
           );
