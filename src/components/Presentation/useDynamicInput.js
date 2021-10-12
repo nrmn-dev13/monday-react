505ebbea38bf99fed651f9f5c90b120cc64a1e1a
normanvnfc <norman@venuerific.com>
2021-10-12 20:23:55 +0700

    Create custom hooks

--- src/components/Presentation/useDynamicInput.js
+++ src/components/Presentation/useDynamicInput.js
@@ -1,27 +1,25 @@
 import { useState } from "react";
 
 export const useDynamicInput = (initialValues) => {
-  const [multipleValues, setMultipleValues] = useState(initialValues)
-  const handleRemoveClick = index => {
-    const list = [...multipleValues];
-    list.splice(index, 1);
-    setMultipleValues(list);
-  };
-  // handle click event of the Add button
-  const handleAddClick = () => {
+  const [multipleValues, setMultipleValues] = useState(initialValues);
+
+  const handleAddClicks = () => {
     setMultipleValues([...multipleValues, { initialValues }]);
   };
+  const handleRemoveClicks = index => {
+    const curField = [...multipleValues];
+    curField.splice(index, 1);
+    setMultipleValues(curField);
+  };  
 
   return [
     multipleValues,
     (e, index) => {
-      setMultipleValues({
-        ...multipleValues[index],
-        [e.target.name]: e.target.value
-      })
+      const curValues = [...multipleValues]
+      curValues[index][e.target.name] = e.target.value
+      setMultipleValues(curValues);
     },
-    handleRemoveClick,
-    handleAddClick
+    handleAddClicks,
+    handleRemoveClicks    
   ]
 }
-
