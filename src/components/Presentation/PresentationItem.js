bc33b6cda36517a7541ef208b751c0cbc1bc7b15
normanvnfc <norman@venuerific.com>
2021-10-15 11:16:35 +0700

    Minor styling

--- src/components/Presentation/PresentationItem.js
+++ src/components/Presentation/PresentationItem.js
@@ -1,14 +1,14 @@
 const PresentationItem = (props) => {
   const { title, achievements, divisions, roles, wips, finisheds, goals } = props;
-  const roleValues = roles.map((role, index) => {return (<p key={index} >Role: {role}</p>)})
-  const goalsValue = goals.map((goal, index) => {return (<p key={index} >Goals: {goal}</p>)})
-  const finishedValue = finisheds.map((finished, index) => {return (<p key={index} >finished: {finished}</p>)})
-  const wipValues = wips.map((wip, index) => {return (<p key={index} >Work In Progress: {wip}</p>)})
+  const roleValues = roles.map((role, index) => {return (<h4 key={index} ><p>Role: {role}</p></h4>)})
+  const goalsValue = goals.map((goal, index) => {return (<h4 key={index} ><p>Goals: {goal}</p></h4>)})
+  const finishedValue = finisheds.map((finished, index) => {return (<h4 key={index} ><p>Finished: {finished}</p></h4>)})
+  const wipValues = wips.map((wip, index) => {return (<h4 key={index} ><p>Work In Progress: {wip}</p></h4>)})
   return (
     <li>
       {/* <p>{props.title}</p>
       <p>{props.address}</p> */}
-      <p>{title}</p>
+      <h2>{title}</h2>
       <h4>Achievement</h4>
       {achievements.map((achievement, index) =>{
         return (
